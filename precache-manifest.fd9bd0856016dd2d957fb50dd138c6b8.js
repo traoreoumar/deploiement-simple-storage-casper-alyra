4a88aaa8711697c795ea5ca194e3ceb5e863eefa
Oumar Traore <oumar.traore.dev@gmail.com>
2021-06-24 18:07:09 +0200

    Updates

---
+++ precache-manifest.fd9bd0856016dd2d957fb50dd138c6b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc5aae6411cf67c7d05d2dbf9f1afc7",
+    "url": "/deploiement-simple-storage-casper-alyra/index.html"
+  },
+  {
+    "revision": "675cf0bc67e20b12bbf6",
+    "url": "/deploiement-simple-storage-casper-alyra/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a261235400c1540e3549",
+    "url": "/deploiement-simple-storage-casper-alyra/static/js/2.e9829414.chunk.js"
+  },
+  {
+    "revision": "675cf0bc67e20b12bbf6",
+    "url": "/deploiement-simple-storage-casper-alyra/static/js/main.e0e192b5.chunk.js"
+  },
+  {
+    "revision": "6c6f1141ff563bee2192",
+    "url": "/deploiement-simple-storage-casper-alyra/static/js/runtime-main.b9e6726f.js"
+  }
+]);
