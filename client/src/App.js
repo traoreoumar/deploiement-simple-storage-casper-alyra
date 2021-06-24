39e2c656c4514cb6ec0db58f1db1ff160fc1d132
Oumar Traore <oumar.traore.dev@gmail.com>
2021-06-24 16:50:28 +0200

    init de l'exercice 'Déploiement DApp sur heroku et GitHub Pages'

---
+++ client/src/App.js
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import getWeb3 from "./getWeb3";
+
+import "./App.css";
+
+class App extends Component {
+  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        SimpleStorageContract.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({ web3, accounts, contract: instance }, this.runExample);
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+  };
+
+  runExample = async () => {
+    const { accounts, contract } = this.state;
+
+    // Get the value from the contract to prove it worked.
+    const response = await contract.methods.get().call();
+
+    // Update state with the result.
+    this.setState({ storageValue: response });
+  };
+
+  set = async() => {
+    const { accounts, contract } = this.state;
+    const storage = this.storage.value;
+    
+    // Interaction avec le smart contract pour ajouter un compte 
+    await contract.methods.set(storage).send({from: accounts[0]});
+    // Récupérer la liste des comptes autorisés
+    this.runExample();
+    this.storage.value = 0;
+  }
+
+  render() {
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contract...</div>;
+    }
+    return (
+      <div className="App">
+        <h1>Good to Go!</h1>
+        <p>Your Truffle Box is installed and ready.</p>
+        <h2>Smart Contract Example</h2>
+        <p>
+          If your contracts compiled and migrated successfully, below will show
+          a stored value of 5 (by default).
+        </p>
+        <p>
+          Try changing the value stored on <strong>line 40</strong> of App.js.
+        </p>
+        <div>The stored value is: {this.state.storageValue}</div>
+        <div>Set a new stored value: 
+              <div className="form-group mr-sm-2">
+                <input
+                  id="storage"
+                  type="number"
+                  ref={(input) => { this.storage = input }}
+                  className="form-control"
+                required />
+              </div>  
+              <button onClick = { this.set } className="btn btn-primary">Set</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
